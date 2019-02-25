0e573c2bd7a83549d0945829accb6c54e46f2c35
Clare McEwan <cfmcewan@gmail.com>
2019-02-25 22:38:13 +0000

    initial commit

---
+++ src/containers/ViewMore.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ViewMore extends Component {
+    render() {
+    return(
+      <div className="view-more">
+      <button><a href="https://www.imdb.com/calendar/?region=gb">View more upcoming releases</a></button>
+      </div>
+    );
+  }
+}
+
+
+export default ViewMore;
